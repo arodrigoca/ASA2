2887808b8c409268b27afa4550b8b68070656997
Adrian Rodrigo Castillo <arodrigo@gamma05.aulas.gsyc.urjc.es>
2018-10-31 14:50:03 +0100

    update

--- P4/factorialoptimizado.asm
+++ P4/factorialoptimizado.asm
@@ -10,28 +10,34 @@ msg: .asciiz "The factorial of 3 is: "
 main: la $a0, msg
 li $v0, 4 # Load syscall print-string into $v0
 syscall # Make the syscall
-li $a0,3 # Put argument (3) in $a0
+li $a0,4 # Put argument (3) in $a0
 jal fact # Call factorial function
 move $a0, $v0 # Move fact result in $a0
 li $v0, 1 # Load syscall print-int into $v0
 syscall # Make the syscall
 li $v0, 10 # Load syscall exit into $v0
 syscall # Make the syscall
+move $v0, $a0
 
 ## fact
 ## Registers used:
 ## $a0
 fact:
+ bgtz $v0, fact_recur # fact(n-1)
+ li $v0, 1
+ b return_fact
+ 
+fact_recur:
+
+#------------esto es lo relacionado con la creacion de pila y correspondientes operaciones de guardado
 subu $sp, $sp, 32 # Stack frame is 32 bytes long
 sw $ra, 20($sp) # Save return address
 sw $fp, 16($sp) # Save frame pointer
 addiu $fp, $sp, 28 # Set up frame pointer
 sw $a0, 0($fp) # Save argument (n)
-lw $v0, 0($fp)
- bgtz $v0, fact_recur # fact(n-1)
- li $v0, 1
- b return_fact
-fact_recur:
+lw $v0, 0($fp) #Esto coge lo que valía a0 y lo deja en v0
+#---------------------------------------------------------------------------------------------------
+
 lw $v1, 0($fp) # Load n
  subu $v0, $v1, 1 # Compute n - 1
  move $a0, $v0 # Move value to $a0
@@ -39,8 +45,10 @@ lw $v1, 0($fp) # Load n
 
  lw $v1, 0($fp) # Load n
  mul $v0, $v0, $v1 # Compute fact(n-1) * n
+ 
+ 
 return_fact:
 lw $ra, 20($sp)
  lw $fp, 16($sp)
- addiu $sp, $sp, 32
+ addiu $sp, $sp, 32 #liberar la memoria dejando el stack pointer al principio de la pila para fact 0
  jr $ra
--- P4/factorialoptimizadoclase.asm
+++ P4/factorialoptimizadoclase.asm
@@ -22,23 +22,29 @@ syscall # Make the syscall
 ## Registers used:
 ## $a0
 fact:
+
+bgt $a0, 1, fact2 #comprobar la condición antes de crear pila
+li $v0, 1
+jr $ra
+
+fact2:
+
 subu $sp, $sp, 32 # Stack frame is 32 bytes long
 sw $ra, 20($sp) # Save return address
 sw $fp, 16($sp) # Save frame pointer
 addiu $fp, $sp, 28 # Set up frame pointer
-sw $a0, 0($fp) # Save argument (n)
-lw $v0, 0($fp)
- bgtz $v0, fact_recur # fact(n-1)
- li $v0, 1
- b return_fact
-fact_recur:
-lw $v1, 0($fp) # Load n
- subu $v0, $v1, 1 # Compute n - 1
- move $a0, $v0 # Move value to $a0
+sw $a0, 0($fp) # Save argument (n) en la pila
+
+#calcula el factorial de n-1
+#lw $v1, 0($fp) # Load n
+ subu $a0, $a0, 1 # Compute n - 1
  jal fact
 
+#recuperamos n
  lw $v1, 0($fp) # Load n
  mul $v0, $v0, $v1 # Compute fact(n-1) * n
+ 
+ 
 return_fact:
 lw $ra, 20($sp)
  lw $fp, 16($sp)
