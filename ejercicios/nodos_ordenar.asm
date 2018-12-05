4eb77a9982a40ebb54819ced8ec5964cf9ef971f
Adrian Rodrigo Castillo <arodrigo@gamma25.aulas.gsyc.urjc.es>
2018-12-05 14:54:02 +0100

    update

--- ejercicios/nodos_ordenar.asm
+++ ejercicios/nodos_ordenar.asm
@@ -10,7 +10,7 @@ main:
 	move $a0, $s0
 	li $a1, 0
 	jal node_create
-	move $s0, $v0
+	move $s0, $v0 #update root node
 	
 	main_loop:
 		li $v0, 5
@@ -19,18 +19,13 @@ main:
 		move $a1, $v0 #move the value to the second argument, a1
 		move $a0, $s0 #move the root node to first argument, a0
 		jal insert_node
+		#now v0 is 0 if root node didnt change or v0 will contain new root node
+		beq $v0, 0, main_loop
 		move $s0, $v0 #update root node
-		
-		li $v0, 1
-		lw $a0, 0($s0)
-		syscall
-		
-		
-		li $v0, 1
-		lw $a0, 4($s0)
-		syscall
-		
+
 		b main_loop
+		
+
 
 node_create: #a0 is the value of the node. a1 is the next pointer. Returns address of created node
 
@@ -42,24 +37,23 @@ node_create: #a0 is the value of the node. a1 is the next pointer. Returns addre
 	sw $a0, 4($fp)
 	
 	li $v0, 9  #invoke heap allocation
-	li $a0, 12
+	li $a0, 8
 	syscall
-	
-	move $t0, $v0 #store the memory address the SO gave in t0
 	
 	lw $a0, 4($fp) #restore a0
 	lw $a1, 0($fp) #restore a1, but it's not necessary
 	
-	sw $a0, 0($t0) #store node data on the heap
-	sw $a1, 4($t0)
-	beq $t0, 0, end_read
+	
+	beq $v0, 0, end_read
+	sw $a0, 0($v0) #store node data on the heap
+	sw $a1, 4($v0)
 	
 	lw $ra, 16($sp) #end subrutine
 	lw $fp, 20($sp)
 	addiu $sp, $sp, 32
 	jr $ra
 
-
+#--node_t*insert_node(node_t*first, int val)
 insert_node: #a0 is the address of the root node. a1 is the value of the new node to insert. returns 0 if root node changed. othersie, returns node address.
 
 	subu $sp, $sp, 32 # Stack frame is 32 bytes long
@@ -69,47 +63,65 @@ insert_node: #a0 is the address of the root node. a1 is the value of the new nod
 	sw $a1, 0($fp) #store arguments
 	sw $a0, 4($fp)
 	
-	#Prepare values for node_create. If the new value is smaller than the root, root pointer now points new value 
-	
-	lw $t0, 0($a0)
-	move $t1, $a1
-	
-	bgt $t1, $t0, new_value_bigger
-	
-	#--------------------------case when new value is smaller than the root node
-	move $a0, $t1 # in this case, create node with null pointer to next
-	li $a1, 0
-	jal node_create #this returned us the address of the new node in $v0
-	lw $a0, 4($fp)
-	lw $a1, 0($fp)
-	sw $v0, 4($s0)
-	move $v0, $a0
-	b end_routine
-	
-	#--------------------------case when the new valye is bigger than the root node
-	
-	new_value_bigger:
+	#create new node
 	
 	move $a0, $a1
-	move $a1, $s0
+	li $a1, 0
 	jal node_create
-	lw $a0, 4($fp)
+	#v0 pointer to created node
+	lw $a0, 4($fp) 
 	lw $a1, 0($fp)
-	#in v0 will be the new node address
+	#case 1, new node is now the first one
+	
+	lw $t0, 0($a0)
+	bgt $a1, $t0, case1
+	
+	
+	case1:
 		
-	#--------------------------------
+		sw $a0, 4($v0)
+		#return v0 which is the new node
+		lw $ra, 16($sp) #end subrutine
+		lw $fp, 20($sp)
+		addiu $sp, $sp, 32
+		jr $ra
 	
-	end_routine:
-	
+		
 	lw $ra, 16($sp) #end subrutine
 	lw $fp, 20($sp)
 	addiu $sp, $sp, 32
 	jr $ra
 
 
+#--void print(node_t*first)
+#--a0: first
+
+print:
+
+	bne $a0, 0, print_rec
+
+	jr $ra
+
+print_rec:
+	
+	add $sp, $sp, -32
+	sw $ra, 20($sp)
+	sw $fp, 16($sp)
+	
+	#comprobar si first es 0 retornar
+	
+	
+	lw $a0, 0($a0)
+	li $v0, 1
+	syscall
+	
+	
+	jr $ra
+
 
 end_read:
-
+	
+	
 	li $v0, 10
 	syscall
 
